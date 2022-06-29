1b04d6a8294256a9c6b2e9e96a15a73a43ab6acf
wanli <kanzihuang@qq.com>
2022-06-29 19:08:17 +0800

    fix copy-image.sh to support immutable image digest

--- copy-image.sh
+++ copy-image.sh
@@ -12,22 +12,5 @@ source "$(dirname $0)/utils.sh"
 
 for path_original in "$@"; do
   path_wraped=$(wrap_image_path $path_original)
-  [[ "$path_wraped" == "$path_original" ]] && continue
-
-  if ! layers_original="$(skopeo inspect -f {{.Layers}} docker://$path_original 2> /dev/null)"; then
-    echo "Failed to inspect source image: $path_original"
-    continue
-  fi
-
-  if ! layers_wraped="$(skopeo inspect -f {{.Layers}} docker://$path_wraped 2> /dev/null)"; then
-    layers_wraped=""
-  fi
-
-  if [[ -z "$layers_wraped" || "$layers_wraped" != "$layers_original" ]]; then
-    skopeo copy docker://$path_original docker://$path_wraped
-    echo "Image '$path_original' was copied to hub.hocker.com/$account"
-  else
-    echo "Image '$path_original' already exists on hub.hocker.com/$account"
-  fi
-
+  [[ "$path_wraped" != "$path_original" ]] && skopeo copy --all docker://$path_original docker://$path_wraped
 done
