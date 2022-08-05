50d32b4dc04447df1debe1388ee7e8168d15c1ff
wanli <kanzihuang@qq.com>
2022-08-05 19:08:06 +0800

    pause mirroring images

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,11 @@ username=$3
 password=$4
 
 skopeo login --username $username --password $password $registry
-find ./mirror.d/ -type f -exec xargs -a {} ./copy-image.sh $registry $group \;
+
+# mirror the below images
 # xargs -a ./mirror.d/csi/rook/csi-rook-v17 ./copy-image.sh $registry $group
+
+# mirror all images
+# find ./mirror.d/ -type f -exec xargs -a {} ./copy-image.sh $registry $group \;
+
 skopeo logout $registry
